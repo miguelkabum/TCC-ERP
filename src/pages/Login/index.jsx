d497bfa10de6034e38e8919b421191ab13dd2ca5
MIGUEL PEREIRA DE OLIVEIRA <moliveira@labcetec.net>
2024-09-11 17:31:18 -0300

    versão 1

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,18 @@
+<div id="main">
+      <h1>Login</h1>
+      <div id="login">
+        <form action="">
+          <div className="input">
+            <label className="label" htmlFor="">Usuário ou e-mail</label>
+            <input type="text" />
+          </div>
+          <div className="input">
+            <label className="label" htmlFor="">Senha</label>
+            <input type="Password" />
+          </div>
+        </form>
+        <button>Entrar</button>
+        <a href="">Esqueceu sua senha?</a>
+        <a href="">Ainda não tem um cadastro? Inscreva-se agora!</a>
+      </div>
+    </div>
