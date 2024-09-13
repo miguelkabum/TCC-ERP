0b007016bdfaaab8851161a3dd21df1883fbc66c
MIGUEL PEREIRA DE OLIVEIRA <moliveira@labcetec.net>
2024-09-13 17:31:27 -0300

    atuaização 13/09

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import Login from './pages/Login/index.jsx'
+
 export default function App() {
   
   return (
+    <>
+      <Login />
+    </>
     
   )
 }
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,18 +1,55 @@
-<div id="main">
-      <h1>Login</h1>
-      <div id="login">
-        <form action="">
-          <div className="input">
-            <label className="label" htmlFor="">Usuário ou e-mail</label>
+import styles from './style.module.css'
+import imgLogin from '../../assets/imgLogin.jpg'
+
+const Login = () => {
+  return(
+    <div id={styles.main}>
+      <div id={styles.container}>
+
+        <div id={styles.img}>
+          <img src={imgLogin} alt="" />
+        </div>
+
+        <div id="loginParts">
+          <h1>Login</h1>
+
+          <form action="">
+
+            <div className={styles.input}>
+
+            <label 
+            className={styles.textLabel} 
+            htmlFor="">
+              Usuário ou e-mail
+            </label>
+
             <input type="text" />
-          </div>
-          <div className="input">
-            <label className="label" htmlFor="">Senha</label>
+
+            </div>
+
+            <div className={styles.input}>
+
+            <label 
+            className={styles.textLabel} 
+            htmlFor=""
+            >
+              Senha
+            </label>
+
             <input type="Password" />
-          </div>
-        </form>
-        <button>Entrar</button>
-        <a href="">Esqueceu sua senha?</a>
-        <a href="">Ainda não tem um cadastro? Inscreva-se agora!</a>
+
+            </div>
+
+          </form>
+
+          <button>Entrar</button>
+
+          <a href="">Esqueceu sua senha?</a>
+
+          <a href="">Ainda não tem um cadastro? Inscreva-se agora!</a>
+        </div>
       </div>
     </div>
+  )
+}
+export default Login;
