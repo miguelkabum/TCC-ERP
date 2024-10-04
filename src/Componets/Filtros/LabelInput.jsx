a0c8da51091b774427c4bad1406bf2e496bc5c92
MIGUEL PEREIRA DE OLIVEIRA <moliveira@labcetec.net>
2024-10-04 14:23:38 -0300

    Tst 1

--- src/Componets/Filtros/LabelInput.jsx
+++ src/Componets/Filtros/LabelInput.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import styles from "./style.module.css";
 
-const LabelInput = () => {
+const LabelInput = (props) => {
+  // const Label = {
+  //     back-groundColor = "red";
+  // }
   return (
-    <div>LabelInput</div>
-  )
-}
+    <div id={styles.container}>
+      <label htmlFor={props.id}>{props.lbl}</label>
+      <div id="caixa">
+        <select
+          className={styles.select}
+          type="select"
+          name={props.id}
+          id={props.id}
+          value={props.value}
+          // onChange={() => setValue(target.value)}
+        >
+          <option value="">Seleione uma opção</option>
+        </select>
+      </div>
+    </div>
+  );
+};
 
-export default LabelInput
+export default LabelInput;
--- src/Componets/Filtros/index.jsx
+++ src/Componets/Filtros/index.jsx
@@ -1,14 +1,39 @@
 import React from "react";
 import Style from "./style.module.css";
+import LabelInput from "./LabelInput.jsx";
+import { useState } from "react";
 
 const index = () => {
+  const { title, setTitle } = useState("Abroba");
   return (
     <>
       <div className={Style.container}>
-        
+        <div id={Style.titleFiltro}>
+          <span>icone</span>
+          <p>Filtros</p>
+        </div>
+        <div className={Style.selects}>
+          <LabelInput
+            id="title"
+            lbl="Situação "
+            value={title}
+            setValue={setTitle}
+          />
+          <LabelInput
+            id="title"
+            lbl="Vendedor "
+            value={title}
+            setValue={setTitle}
+          />
+          <LabelInput
+            id="title"
+            lbl="Telefone/Celular "
+            value={title}
+            setValue={setTitle}
+          />
+        </div>
       </div>
     </>
   );
 };
-
 export default index;
